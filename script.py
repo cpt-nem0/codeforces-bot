e345c99102da42ed6e4a8d1c35b50fc93ef698c7
cpt-nem0 <rohanya.76426@gmail.com>
2020-05-14 07:12:15 -0700

    initial commit

---
+++ script.py
@@ -0,0 +1,43 @@
+#! /usr/bin/python3.8
+
+import sys
+import os
+
+
+# copy contents of templet to .cpp
+def cpp_file(src):
+    file = open(src, "w")
+    with open(r'/home/cptn3m0/Desktop/codeforces/file creation script/templet.txt', "r") as templet:
+        file.write(templet.read())
+
+    # os.system("geany {}".format(src))
+
+
+def create_file(src):
+    file = open(src, "w+")
+    file.close()
+
+
+try:
+    # file name
+    file_name = str(sys.argv[1])
+    name, ext = file_name.split('.')
+    file = os.getcwd() + '/' + file_name
+    # calling cpp function for templet addition and file creation
+    if ext == 'cpp':
+        confirm = input('Do you want to copy templets?(y/n): ')
+        if confirm.lower() == 'y':
+            cpp_file(file)
+        else:
+            create_file(file)
+
+    # just for now
+    else:
+        create_file(file)
+
+
+except IndexError:
+    print('File must be Enter')
+
+except FileNotFoundError:
+    print("'templet.txt' not Found.")
